7aec8399d714b4e281277b541ae43ca01e0c5ad3
MIRAN <kmr@kcnet.co.kr>
2021-02-07 22:53:43 +0900

    CORS annotation 추가

--- src/main/java/com/kcnet/bowingapijava/api/freeboard/FreeBoardController.java
+++ src/main/java/com/kcnet/bowingapijava/api/freeboard/FreeBoardController.java
@@ -1,11 +1,13 @@
 package com.kcnet.bowingapijava.api.freeboard;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class FreeBoardController {
 
@@ -13,7 +15,7 @@ public class FreeBoardController {
 	public FreeBoardDto getFreeBoard() {
 		return FreeBoardDto.builder().id("001")
 			.authorName("관리자")
-			.content("자유게시판입니다.")
+			.content("자유게시판입니다.내용내용내용")
 			.date("2021-01-01")
 			.title("[공지사항]욕설금지합니다.")
 			.build();
@@ -24,16 +26,20 @@ public class FreeBoardController {
 
 		return Arrays.asList(
 			FreeBoardDto.builder().id("001")
+				.number("1")
 				.authorName("관리자")
 				.content("자유게시판입니다.")
 				.date("2021-01-01")
 				.title("[공지사항]욕설금지합니다.")
+				.views("100")
 				.build(),
 			FreeBoardDto.builder().id("002")
+				.number("2")
 				.authorName("김미란")
 				.content("반갑습니다.가입인사 드려요")
 				.date("2021-01-02")
 				.title("가입인사 드려요")
+				.views("200")
 				.build()
 		);
 	}
